691518ee6f66bec8a491f31d076181170aac513c
Ana Clara Zampier Teixeira <anateixeira-ieg@instituto.ad>
2023-11-08 20:32:27 -0300

    carrinho adapter

--- app/src/main/java/com/example/busco/Fragments/principal_fragment.java
+++ app/src/main/java/com/example/busco/Fragments/principal_fragment.java
@@ -1,11 +1,13 @@
 package com.example.busco.Fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,12 +16,32 @@ import androidx.fragment.app.Fragment;
 
 import com.example.busco.Api.Models.Usuarios;
 import com.example.busco.R;
+import com.example.busco.WebViewActivity;
 import com.google.gson.Gson;
 
 public class principal_fragment extends Fragment {
+    private LinearLayout acoes;
+    private LinearLayout quemSomos;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.principal_fragment, container, false);
+
+        acoes = view.findViewById(R.id.linearLayout7);
+        quemSomos = view.findViewById(R.id.linearLayout72);
+
+        acoes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirLandPage();
+            }
+        });
+
+        quemSomos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirLandPage();
+            }
+        });
 
         Gson gson = new Gson();
         SharedPreferences sharedPreferences = requireActivity().getSharedPreferences("UserData", Context.MODE_PRIVATE);
@@ -35,5 +57,10 @@ public class principal_fragment extends Fragment {
         nomeCliente.setText(mensagem);
 
         return view;
+    }
+
+    private void abrirLandPage() {
+        Intent intent = new Intent(requireContext(), WebViewActivity.class);
+        startActivity(intent);
     }
 }
